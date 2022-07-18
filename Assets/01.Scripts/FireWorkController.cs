c443237ec24da9565f44f914f0f401950fa91ce7
moonkanghyuck <moonkanghyuck@gmail.com>
2022-07-19 01:52:28 +0900

    07/19_불꽃놀이 이펙트 제작

--- Assets/01.Scripts/FireWorkController.cs
+++ Assets/01.Scripts/FireWorkController.cs
@@ -7,10 +7,32 @@ public class FireWorkController : MonoBehaviour
 {
 	[SerializeField]
 	private VisualEffect _visualEffect = null;
+	[SerializeField]
+	private float _rate = 0f;
+
 
 	private void Start()
 	{
-		_visualEffect.SetFloat("Rate", 5f);
+		UpdateRate();
+	}
+
+
+	/// <summary>
+	/// 주기 설정
+	/// </summary>
+	public void UpdateRate()
+	{
+		_visualEffect.SetFloat("Rate", _rate);
+	}
+
+	/// <summary>
+	/// 주기 증가
+	/// </summary>
+	/// <param name="add"></param>
+	public void UpdateRate(float add)
+	{
+		_rate += add;
+		UpdateRate();
 	}
 
 }
