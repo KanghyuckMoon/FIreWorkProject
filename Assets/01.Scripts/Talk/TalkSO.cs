31779b7f591a5f02c495436201016d5f4520e9d0
bossxz <76054436+bossxz@users.noreply.github.com>
2022-08-22 11:07:49 +0900

    08/22_대사 데이터 제작

--- Assets/01.Scripts/Talk/TalkSO.cs
+++ Assets/01.Scripts/Talk/TalkSO.cs
@@ -5,5 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TalkSO", menuName = "ScriptableObject/TalkSO")]
 public class TalkSO : ScriptableObject
 {
-	public List<string> content = new List<string>();
+	public List<TalkData> talkDatas = new List<TalkData>();
+}
+
+[System.Serializable]
+public class TalkData
+{
+	public enum TalkObject
+	{
+		None,
+		Player,
+		Other,
+	}
+
+	public TalkObject talkObject;
+	public string othername;
+	public string content;
 }
