ac98d071040dd08c0d1fead47fd5f2fc928c184e
bossxz <moonkanghyuck@gmail.com>
2022-08-25 01:55:11 +0900

    08/24_오브젝트 확대 기능 제작

--- Assets/01.Scripts/Input/CameraController.cs
+++ Assets/01.Scripts/Input/CameraController.cs
@@ -57,8 +57,20 @@ public class CameraController : MonoBehaviour
 
     private void CameraPositionSetting()
     {
-        _moveVector = transform.position + transform.right * (_xMoveInput * _moveSpeed * Time.deltaTime) + transform.up * (_yMoveInput * _moveSpeed * Time.deltaTime) + transform.forward* _distance *_zoomSpeed;
+        _moveVector = transform.position + transform.right * (_xMoveInput * _moveSpeed * Time.deltaTime) + transform.up * (_yMoveInput * _moveSpeed * Time.deltaTime) + (transform.forward* _distance *_zoomSpeed * Time.deltaTime);
         transform.position = Vector3.SmoothDamp(transform.position, _moveVector, ref _velocity, _smoothTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-_yRotationInput, _xRotationInput, 0), _smoothTime);;
+    }
+
+    /// <summary>
+    /// Å¸°Ù ¼³Á¤
+    /// </summary>
+    /// <param name="targetTransform"></param>
+    public void SetTarget(Transform targetTransform)
+	{
+        _xMoveInput = 0;
+        _yMoveInput = 0;
+        transform.position = targetTransform.position + -transform.forward * 9;
+
     }
 }
--- Assets/01.Scripts/Input/ObjectSelector.cs
+++ Assets/01.Scripts/Input/ObjectSelector.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class ObjectSelector : MonoBehaviour
 {
+	[SerializeField] private CameraController _camera;
+
+	private void Update()
+	{
+		Click();
+	}
+
+	private void Click()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			CheckObject();
+		}
+
+	}
+
+	private void CheckObject()
+	{
+		Vector3 originPosition = _camera.transform.position;
+		Vector3 direction = _camera.transform.forward;
+		RaycastHit hit;
+
+		if (Physics.Raycast(originPosition, direction, out hit))
+		{
+			if(hit.collider.gameObject.layer == 10)
+			{
+				_camera.SetTarget(hit.collider.transform);
+			}
+		}
+	}
 
 }
