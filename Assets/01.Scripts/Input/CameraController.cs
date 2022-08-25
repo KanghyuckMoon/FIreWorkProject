a5991151cb5c0ccda82f10cbaff819692b41a10c
meng-joo <81345410+meng-joo@users.noreply.github.com>
2022-08-25 16:24:15 +0900

    input 키보드만 바꿔봄

--- Assets/01.Scripts/Input/CameraController.cs
+++ Assets/01.Scripts/Input/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Vector3 _velocity;
     float _yRotationInput = 0f;
     float _xRotationInput = 0f;
+
+    Vector3 inputVal = Vector3.zero;
+
     float _yMoveInput = 0f;
     float _xMoveInput = 0f;
     Vector3 _moveVector = Vector3.zero;
@@ -21,8 +25,14 @@ public class CameraController : MonoBehaviour
     {
         ZoomInOut();
         MoveMouseCamera();
-        MoveKeyboardCamera();
+        //MoveKeyboardCamera();
         CameraPositionSetting();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector3 input = context.ReadValue<Vector3>();
+        inputVal = input;
     }
 
     private void MoveMouseCamera()
@@ -44,11 +54,11 @@ public class CameraController : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
         }
     }
-    private void MoveKeyboardCamera()
-    {
-        _xMoveInput = Input.GetAxis("Horizontal");
-        _yMoveInput = Input.GetAxis("Vertical");
-    }
+    //private void MoveKeyboardCamera()
+    //{
+    //    _xMoveInput = Input.GetAxis("Horizontal");
+    //    _yMoveInput = Input.GetAxis("Vertical");
+    //}
 
     private void ZoomInOut()
 	{
@@ -57,7 +67,7 @@ public class CameraController : MonoBehaviour
 
     private void CameraPositionSetting()
     {
-        _moveVector = transform.position + transform.right * (_xMoveInput * _moveSpeed * Time.deltaTime) + transform.up * (_yMoveInput * _moveSpeed * Time.deltaTime) + (transform.forward* _distance *_zoomSpeed * Time.deltaTime);
+        _moveVector = transform.position + (inputVal * _moveSpeed * Time.deltaTime); //transform.right * (_xMoveInput * _moveSpeed * Time.deltaTime) + transform.up * (_yMoveInput * _moveSpeed * Time.deltaTime) + (transform.forward* _distance *_zoomSpeed * Time.deltaTime);
         transform.position = Vector3.SmoothDamp(transform.position, _moveVector, ref _velocity, _smoothTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-_yRotationInput, _xRotationInput, 0), _smoothTime);;
     }
