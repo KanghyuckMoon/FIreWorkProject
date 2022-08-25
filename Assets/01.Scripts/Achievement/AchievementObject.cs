1cbae049c9b3d5276085d1fc103463cf3a881cb2
bossxz <moonkanghyuck@gmail.com>
2022-08-25 12:26:36 +0900

    08/24_모션을 더욱 매끄럽게 제작함

--- Assets/01.Scripts/Achievement/AchievementObject.cs
+++ Assets/01.Scripts/Achievement/AchievementObject.cs
@@ -53,9 +53,9 @@ public class AchievementObject : MonoBehaviour, Observer
 		Vector3 changePosition = originPosition + new Vector3(0, 3, 0);
 		Vector3 originSize = transform.localScale;
 		Vector3 changeSize = originSize * 1.5f;
-		transform.localScale = changeSize;
+		transform.localScale = originSize * 0.001f;
 		transform.position = changePosition;
-		transform.DOMoveY(originPosition.y, 1).OnComplete(() => _effectObject?.SetActive(false));
-		transform.DOScale(originSize, 1);
+		transform.DOScale(changeSize, 0.5f).SetEase(Ease.OutQuart).OnComplete(() => transform.DOScale(originSize, 1).SetEase(Ease.InExpo));
+		transform.DOMoveY(originPosition.y, 1).SetDelay(0.5f).OnComplete(() => _effectObject?.SetActive(false)).SetEase(Ease.InExpo);
 	}
 }
