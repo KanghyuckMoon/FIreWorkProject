d5499a431dbdc9e4980db4225b8010b2aa9c2eba
moonkanghyuck <moonkanghyuck@gmail.com>
2022-09-15 21:35:19 +0900

    09/15_업적데이터 수정

--- Assets/01.Scripts/Achievement/AchievementDataSO.cs
+++ Assets/01.Scripts/Achievement/AchievementDataSO.cs
@@ -7,9 +7,20 @@ public class AchievementDataSO : ScriptableObject
 {
 	public List<AchievementData> _achievementDatas = new List<AchievementData>();
 
+	public Sprite _debugChangeSprite;
+
 	public AchievementData GetAchievementData(int itemCode)
 	{
 		return _achievementDatas.Find(x => x._achievementCode == itemCode);
+	}
+
+	[ContextMenu("AllChangeSprite")]
+	public void AllChangeSprite()
+	{
+		for(int i = 0; i < _achievementDatas.Count; ++i)
+		{
+			_achievementDatas[i]._sprite = _debugChangeSprite;
+		}
 	}
 
 }
@@ -23,5 +34,6 @@ public class AchievementData
 	public string _functionName;
 	public string _functionParameter;
 	public bool _isCantView = false;
+	public Sprite _sprite = null;
 }
 
